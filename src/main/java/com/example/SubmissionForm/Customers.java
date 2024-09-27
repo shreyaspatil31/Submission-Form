d713cde21830428b800918804bed1bc1f7a06f77
Shreyas Patil <140237535+shreyaspatil31@users.noreply.github.com>
2024-09-28 00:19:25 +0530

    Update Customers.java

--- src/main/java/com/example/SubmissionForm/Customers.java
+++ src/main/java/com/example/SubmissionForm/Customers.java
@@ -1,36 +1,40 @@
 package com.example.SubmissionForm;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Customers {
-	@Id
-	private int cid;
-	private String cname;
-	private String cmail;
-	
-	public int getCid() {
-		return cid;
-	}
-	public void setCid(int cid) {
-		this.cid = cid;
-	}
-	public String getCname() {
-		return cname;
-	}
-	public void setCname(String cname) {
-		this.cname = cname;
-	}
-	public String getCmail() {
-		return cmail;
-	}
-	public void setCmail(String cmail) {
-		this.cmail = cmail;
-	}
-	@Override
-	public String toString() {
-		return "Controllers [cid=" + cid + ", cname=" + cname + ", cmail=" + cmail + "]";
-	}
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cid;
+    private String cname;
+    private String cmail;
+    
+    // Getters and Setters
+    public int getCid() {
+        return cid;
+    }
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+    public String getCname() {
+        return cname;
+    }
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+    public String getCmail() {
+        return cmail;
+    }
+    public void setCmail(String cmail) {
+        this.cmail = cmail;
+    }
+    
+    @Override
+    public String toString() {
+        return "Customers [cid=" + cid + ", cname=" + cname + ", cmail=" + cmail + "]";
+    }
 }
